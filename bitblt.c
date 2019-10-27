9ba654faf56f1dce1bc942271b871636ecd8f0cb
chenk <chenk@apical.com.cn>
2019-10-27 17:47:23 +0800

    implement bar function.

--- bitblt.c
+++ bitblt.c
@@ -5,13 +5,13 @@
 
 /* 内部函数实现 */
 /* 位块传送的区域裁剪函数 */
-static BOOL bltclip(BMP *dstpb, int *dstx, int *dsty, RECT *clipper,
+static BOOL bltclip(BMP *dstpb, int *dstx, int *dsty,
                     BMP *srcpb, int *srcx, int *srcy, int *w, int *h)
 {
-    int dst_clip_l = clipper ? clipper->left  : 0;
-    int dst_clip_t = clipper ? clipper->top   : 0;
-    int dst_clip_r = clipper ? clipper->right : dstpb->width  - 1;
-    int dst_clip_b = clipper ? clipper->bottom: dstpb->height - 1;
+    int dst_clip_l = dstpb->clipper.left;
+    int dst_clip_t = dstpb->clipper.top;
+    int dst_clip_r = dstpb->clipper.right;
+    int dst_clip_b = dstpb->clipper.bottom;
     if (dst_clip_l < 0) dst_clip_l = 0;
     if (dst_clip_t < 0) dst_clip_t = 0;
     if (dst_clip_r > dstpb->width ) dst_clip_r = dstpb->width - 1;
@@ -53,20 +53,45 @@ static BOOL bltclip(BMP *dstpb, int *dstx, int *dsty, RECT *clipper,
 }
 
 /* 函数实现 */
-void bitblt(BMP *dstpb, int dstx, int dsty, RECT *clipper, BMP *srcpb, int srcx, int srcy, int srcw, int srch)
+void bitblt(BMP *dstpb, int dstx, int dsty, BMP *srcpb, int srcx, int srcy, int srcw, int srch)
 {
     DWORD *dst, *src;
-
     if (srcw == -1) srcw = srcpb->width ;
     if (srch == -1) srch = srcpb->height;
-    if (!bltclip(dstpb, &dstx, &dsty, clipper, srcpb, &srcx, &srcy, &srcw, &srch)) return;
+    if (!bltclip(dstpb, &dstx, &dsty, srcpb, &srcx, &srcy, &srcw, &srch)) return;
     src = srcpb->pdata + srcy * srcpb->width + srcx;
     dst = dstpb->pdata + dsty * dstpb->width + dstx;
-    do {
+    while (srch--) {
         memcpy(dst, src, srcw * sizeof(DWORD));
         src += srcpb->width;
         dst += dstpb->width;
-    } while (--srch);
+    }
+}
+
+void bar(BMP *pb, int x, int y, int w, int h, int color)
+{
+    DWORD *dst;
+    int    i;
+    if (w == -1) w = pb->width ;
+    if (h == -1) h = pb->height;
+    if (x < pb->clipper.left) {
+        w-= pb->clipper.left - x;
+        x = pb->clipper.left;
+    }
+    if (y < pb->clipper.top) {
+        h-= pb->clipper.top - y;
+        y = pb->clipper.top;
+    }
+    if (x > pb->clipper.right || y > pb->clipper.bottom) return;
+    if (x + w > pb->clipper.right + 1) w = pb->clipper.right + 1 - x;
+    if (y + h > pb->clipper.bottom+ 1) h = pb->clipper.bottom+ 1 - y;
+    dst = pb->pdata + y * pb->width + x;
+    while (h--) {
+        for (i=0; i<w; i++) {
+            *dst++ = color;
+        }
+        dst += pb->width - w;
+    }
 }
 
 #else
